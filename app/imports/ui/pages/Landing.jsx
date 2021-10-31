79d04c9fd49fa19dbd61b8d578e66f6330bae660
heleeha <helene.e.h@hotmail.com>
2021-10-30 17:25:52 -1000

    landing page mockup finished in 19 minutes

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
+      <div className="digits-landing-background">
+        <Grid id='landing-page' container centered stackable columns={3}>
+          <Grid.Column textAlign='center'>
+            <Icon size="huge" name="users"/>
+            <Header as='h1'>Multiple Users</Header>
+            <Header as='h3'>This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.</Header>
+          </Grid.Column>
+          <Grid.Column textAlign='center'>
+            <Icon size="huge" name="file alternate"/>
+            <Header as='h1'>Contact Details</Header>
+            <Header as='h3'>For each contact you have to save their name, address, and phone number.</Header>
+          </Grid.Column>
+          <Grid.Column textAlign='center'>
+            <Icon size="huge" name="checked calendar"/>
+            <Header as='h1'>Timestamped Notes</Header>
+            <Header as='h3'>Each time you make contact with a contact, you can write a note to summarize the conversation. This note is saved along with a timestamp with the contact.</Header>
+          </Grid.Column>
+        </Grid>
+      </div>
     );
   }
 }
